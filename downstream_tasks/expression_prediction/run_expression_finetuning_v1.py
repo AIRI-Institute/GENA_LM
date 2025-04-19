1711278ac9d37ed71173ae6a2b66b986188931f5
sb-vsfishman <minja-f@ya.ru>
2025-04-19 22:42:27 +0300

    Add option to save predictions and labels

--- downstream_tasks/expression_prediction/run_expression_finetuning_v1.py
+++ downstream_tasks/expression_prediction/run_expression_finetuning_v1.py
@@ -47,6 +47,7 @@ torch.cuda.set_device(hvd.local_rank())
 
 parser = HfArgumentParser(TrainerArgs)
 parser.add_argument('--experiment_config', type=str, help='path to the experiment config') 
+parser.add_argument('--save_predictions', type=bool, default=False, help='save predictions to file')
 
 def main():
 
@@ -334,7 +335,6 @@ def main():
         'dataset_description' : batch['dataset_description'] }
         return result
        
-
     def keep_for_metrics_fn(batch, output):
         predictions_segm = [[el.detach().cpu() for el in s] for s in output['logits_segm']]
         labels_segm = [[el.detach().cpu() for el in s] for s in output['labels_reshaped']]
@@ -386,116 +386,119 @@ def main():
         data['_count'] = torch.sum(torch.ones_like(target), dim=reduce_dims).unsqueeze(0)
         return data
 
-
-    def metrics_fn(data):
-        metrics = {}
-    
-        data['_product'] = torch.sum(data['_product'], dim=0)
-        data['_true'] = torch.sum(data['_true'], dim=0)
-        data['_true_squared'] = torch.sum(data['_true_squared'], dim=0)
-        data['_pred'] = torch.sum(data['_pred'], dim=0)
-        data['_pred_squared'] = torch.sum(data['_pred_squared'], dim=0)
-        data['_count'] = torch.sum(data['_count'], dim=0)
+    def make_metrics_fn(model_path, save_predictions=False):
+        def metrics_fn(data):
+            metrics = {}
         
-        true_mean = data['_true'] / data['_count']
-        pred_mean = data['_pred'] / data['_count']
-    
-        covariance = (data['_product'] - true_mean * data['_pred'] - pred_mean * data['_true'] + data['_count'] * true_mean * pred_mean)
-    
-        true_var = data['_true_squared'] - data['_count'] * torch.square(true_mean)
-        pred_var = data['_pred_squared'] - data['_count'] * torch.square(pred_mean)
-        tp_var = torch.sqrt(true_var) * torch.sqrt(pred_var)
-        corr_coef = covariance / tp_var
-        metrics['pearson_corr'] = corr_coef.item()
-    
-        # Обработка TPM и gene_id
-        tpm_true = data['tpm_true']
-        tpm_preds = data['tpm_preds']
-        gene_id = data['gene_id']
-        keys_id = data['keys_id']
-        dataset_description = data['dataset_description']
-        
-        assert len(tpm_true) == len(tpm_preds) == len(gene_id) == len(keys_id) == len(dataset_description), \
-            f"Mismatch! tpm_true: {len(tpm_true)}, tpm_preds: {len(tpm_preds)}, gene_id: {len(gene_id)}, keys_id {len(keys_id)}, dataset_description {len(dataset_description)}"
-
-        df = pd.DataFrame({
-            'gene_id': gene_id,
-            'cell_type': keys_id,
-            'tpm_true': tpm_true,
-            'tpm_pred': tpm_preds,
-            'dataset_description': dataset_description,
-        })
-        # df.to_csv("/mnt/nfs_dna/aspeedok/github/labels.csv")
-        
-        for dataset_desc in df['dataset_description'].unique():
-            df_dataset = df[df['dataset_description'] == dataset_desc]
+            data['_product'] = torch.sum(data['_product'], dim=0)
+            data['_true'] = torch.sum(data['_true'], dim=0)
+            data['_true_squared'] = torch.sum(data['_true_squared'], dim=0)
+            data['_pred'] = torch.sum(data['_pred'], dim=0)
+            data['_pred_squared'] = torch.sum(data['_pred_squared'], dim=0)
+            data['_count'] = torch.sum(data['_count'], dim=0)
             
-            df_pred = df_dataset.pivot_table(
-                index='gene_id',
-                columns='cell_type',
-                values='tpm_pred',
-                aggfunc='first'
-            )
-            df_true = df_dataset.pivot_table(
-                index='gene_id',
-                columns='cell_type',
-                values='tpm_true',
-                aggfunc='first'
-            )            
-            # df_true.to_csv(f'/mnt/nfs_dna/aspeedok/github/{dataset_desc}_true.csv')
-            # df_pred.to_csv(f'/mnt/nfs_dna/aspeedok/github/{dataset_desc}_pred.csv')
-
+            true_mean = data['_true'] / data['_count']
+            pred_mean = data['_pred'] / data['_count']
+        
+            covariance = (data['_product'] - true_mean * data['_pred'] - pred_mean * data['_true'] + data['_count'] * true_mean * pred_mean)
+        
+            true_var = data['_true_squared'] - data['_count'] * torch.square(true_mean)
+            pred_var = data['_pred_squared'] - data['_count'] * torch.square(pred_mean)
+            tp_var = torch.sqrt(true_var) * torch.sqrt(pred_var)
+            corr_coef = covariance / tp_var
+            metrics['pearson_corr'] = corr_coef.item()
+        
+            # Обработка TPM и gene_id
+            tpm_true = data['tpm_true']
+            tpm_preds = data['tpm_preds']
+            gene_id = data['gene_id']
+            keys_id = data['keys_id']
+            dataset_description = data['dataset_description']
             
-            #Рассчет средней корреляции по генам и типам клеток
-            if not df_true.empty and not df_pred.empty:
-                gene_correlations = []
-                for gene in df_true.index:
-                    gene_true = df_true.loc[gene]
-                    gene_pred = df_pred.loc[gene]
-                    if len(gene_true) > 1 and np.std(gene_true) > 0:
-                        try:
-                            corr = np.corrcoef(gene_true, gene_pred)[0, 1]
-                            if not np.isnan(corr):
-                                gene_correlations.append(corr)
-                        except:
-                            continue
-                
-                cell_correlations = []
-                for cell_type in df_true.columns:
-                    cell_true = df_true[cell_type]
-                    cell_pred = df_pred[cell_type]
-                    if len(cell_true) > 1:
-                        try:
-                            corr = np.corrcoef(cell_true, cell_pred)[0, 1]
-                            if not np.isnan(corr):
-                                cell_correlations.append(corr)
-                        except:
-                            continue
-                
-                if gene_correlations:
-                    metrics[f'pearson_corr_cells_{dataset_desc}'] = float(np.mean(gene_correlations))
-                if cell_correlations:
-                    metrics[f'pearson_corr_genes_{dataset_desc}'] = float(np.mean(cell_correlations))
+            assert len(tpm_true) == len(tpm_preds) == len(gene_id) == len(keys_id) == len(dataset_description), \
+                f"Mismatch! tpm_true: {len(tpm_true)}, tpm_preds: {len(tpm_preds)}, gene_id: {len(gene_id)}, keys_id {len(keys_id)}, dataset_description {len(dataset_description)}"
 
-                #Корреляция только для 5 файлов из borzoi
-                target_genes = ['ENCFF123KIW', 'ENCFF784MDF', 'ENCFF236XOK', 'ENCFF781TTC', 'ENCFF242BWW']
-                target_metrics = calculate_target_genes_metrics(df_true, df_pred, target_genes)
-                metrics[f'pearson_corr_5_cells_borzoi_cells_{dataset_desc}'] = target_metrics['corr_selected_cell_types']
-                metrics[f'pearson_corr_5_cells_borzoi_genes_{dataset_desc}'] = target_metrics['corr_selected_genes']
+            df = pd.DataFrame({
+                'gene_id': gene_id,
+                'cell_type': keys_id,
+                'tpm_true': tpm_true,
+                'tpm_pred': tpm_preds,
+                'dataset_description': dataset_description,
+            })
+            if save_predictions:
+                df.to_csv(os.path.join(model_path, "labels.csv"))
+            
+            for dataset_desc in df['dataset_description'].unique():
+                df_dataset = df[df['dataset_description'] == dataset_desc]
                 
-                #Целевая метрика клетоспецифичности 
-                df_true = df_true.reset_index()
-                df_pred = df_pred.reset_index()
-                score = score_predictions(
-                    df_true, 
-                    df_pred, 
-                    experiment_config.selected_targets_path, 
-                    need_log=False
+                df_pred = df_dataset.pivot_table(
+                    index='gene_id',
+                    columns='cell_type',
+                    values='tpm_pred',
+                    aggfunc='first'
                 )
-                if score:
-                    metrics[f'score_predictions_{dataset_desc}'] = score['deviation_r']
+                df_true = df_dataset.pivot_table(
+                    index='gene_id',
+                    columns='cell_type',
+                    values='tpm_true',
+                    aggfunc='first'
+                )            
+                if save_predictions:
+                    df_true.to_csv(os.path.join(model_path, f"{dataset_desc}_true.csv"))
+                    df_pred.to_csv(os.path.join(model_path, f"{dataset_desc}_pred.csv"))
+                
+                #Рассчет средней корреляции по генам и типам клеток
+                if not df_true.empty and not df_pred.empty:
+                    gene_correlations = []
+                    for gene in df_true.index:
+                        gene_true = df_true.loc[gene]
+                        gene_pred = df_pred.loc[gene]
+                        if len(gene_true) > 1 and np.std(gene_true) > 0:
+                            try:
+                                corr = np.corrcoef(gene_true, gene_pred)[0, 1]
+                                if not np.isnan(corr):
+                                    gene_correlations.append(corr)
+                            except:
+                                continue
+                    
+                    cell_correlations = []
+                    for cell_type in df_true.columns:
+                        cell_true = df_true[cell_type]
+                        cell_pred = df_pred[cell_type]
+                        if len(cell_true) > 1:
+                            try:
+                                corr = np.corrcoef(cell_true, cell_pred)[0, 1]
+                                if not np.isnan(corr):
+                                    cell_correlations.append(corr)
+                            except:
+                                continue
+                    
+                    if gene_correlations:
+                        metrics[f'pearson_corr_cells_{dataset_desc}'] = float(np.mean(gene_correlations))
+                    if cell_correlations:
+                        metrics[f'pearson_corr_genes_{dataset_desc}'] = float(np.mean(cell_correlations))
 
-        return metrics
+                    #Корреляция только для 5 файлов из borzoi
+                    target_genes = ['ENCFF123KIW', 'ENCFF784MDF', 'ENCFF236XOK', 'ENCFF781TTC', 'ENCFF242BWW']
+                    target_metrics = calculate_target_genes_metrics(df_true, df_pred, target_genes)
+                    metrics[f'pearson_corr_5_cells_borzoi_cells_{dataset_desc}'] = target_metrics['corr_selected_cell_types']
+                    metrics[f'pearson_corr_5_cells_borzoi_genes_{dataset_desc}'] = target_metrics['corr_selected_genes']
+                    
+                    #Целевая метрика клетоспецифичности 
+                    df_true = df_true.reset_index()
+                    df_pred = df_pred.reset_index()
+                    score = score_predictions(
+                        df_true, 
+                        df_pred, 
+                        experiment_config.selected_targets_path, 
+                        need_log=False
+                    )
+                    if score:
+                        metrics[f'score_predictions_{dataset_desc}'] = score['deviation_r']
+
+            return metrics
+        return metrics_fn
+    metrics_fn = make_metrics_fn(args.model_path, save_predictions=args.save_predictions)
 
     trainer = Trainer(args, model, optimizer, train_dataloader, valid_dataloader=valid_dataloader,
                       train_sampler=train_sampler, batch_transform_fn=batch_transform_fn,  metrics_fn=metrics_fn, keep_for_metrics_fn=keep_for_metrics_fn)
