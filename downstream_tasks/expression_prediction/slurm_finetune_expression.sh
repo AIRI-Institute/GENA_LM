63371e31886f50a3c1590090b0f86030ccd72f3e
Egor Tsoi <tsoies@hydra-eye1.ipa.icgbio.ru>
2026-01-27 03:25:45 +0700

    adding slurm support for finetune_expression.sh

---
+++ downstream_tasks/expression_prediction/slurm_finetune_expression.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd ../..
+
+export CUDA_HOME="/usr/local/cuda"
+export PATH="/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+export GENALM_HOME="/scratch/tsoies-Expression/GENA_LM"
+
+export CUDA_VISIBLE_DEVICES=0
+
+
+# export TORCH_DISTRIBUTED_DEBUG=DETAIL
+
+#  --multi_gpu \
+
+TBS=1000
+BS=5
+NP=8
+GAS=$(( TBS / (BS * NP) ))
+
+config_name="config_random_borzoi"
+
+GENALM_HOME=$(realpath ..)
+
+srun -c 10 --mem 50G --time 30-0 -p gpu_A100 --gres gpu:1 accelerate launch \
+  --main_process_port 29516 \
+  --num_processes "$NP" \
+  --module downstream_tasks.expression_prediction.run_expression_finetuning_fr_embed \
+  --experiment_config "downstream_tasks/expression_prediction/configs/${config_name}.yaml" \
+  --batch_size "$BS" \
+  --gradient_accumulation_steps "$GAS"
+
+echo "done"
