797212725b60f47f82aa54efd720a50fb5cc9ff1
Аспидова Алена <aspidova@bio-dna.2a2i.local>
2026-06-10 11:14:23 +0300

    dna

--- downstream_tasks/expression_prediction/finetune_expression.sh
+++ downstream_tasks/expression_prediction/finetune_expression.sh
@@ -7,7 +7,7 @@ export CUDA_HOME="$HOME/.local/cuda/"
 export PATH="$HOME/.local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="$HOME/.local/cuda/lib64/:$LD_LIBRARY_PATH"
 
- export CUDA_VISIBLE_DEVICES=0,1
+ export CUDA_VISIBLE_DEVICES=0
 # export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
 #  --multi_gpu \
@@ -17,11 +17,10 @@ BS=2
 NP=2
 GAS=$(( TBS / (BS * NP) ))  
 
-config_name="human_mouse_gradlog"
+config_name="human_mouse_test"
 
 GENALM_HOME=$(realpath ..) accelerate launch \
   --main_process_port 29515 \
-  --multi_gpu \
   --num_processes "$NP" \
   --module downstream_tasks.expression_prediction.run_expression_finetuning_final \
   --experiment_config "downstream_tasks/expression_prediction/configs/${config_name}.yaml" \
